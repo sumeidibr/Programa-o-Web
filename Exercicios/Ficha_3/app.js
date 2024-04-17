476b3abe5a090d96c43247a1d15575b6675cb0ae
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2024-04-17 16:03:07 +0200

    jkj

---
+++ Exercicios/Ficha_3/app.js
@@ -0,0 +1,3 @@
+const hamburger  = document.querySelector(".hamburguer");
+        const nav = document.querySelector(".nav");
+        hamburger.addEventListener("click", () => nav.classList.toggle("active"));
